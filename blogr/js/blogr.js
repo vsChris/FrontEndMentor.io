470483e4df7406c2f5fde634b0285f148ac64104
vsChris <>
2021-06-17 16:04:45 -0400

    Updates made

--- blogr/js/blogr.js
+++ blogr/js/blogr.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav-list');
-const dropdownLinks = document.querySelectorAll('.dropdown');
+const dropdownLinks = document.querySelectorAll('.top-lvl-link');
+const dropdownMenus = document.querySelectorAll('.dropdown-menu');
 
 let menuOpen = false;
+let dropdownOpen = false;
 
 console.log(hamburger);
 console.log(nav);
 console.log(dropdownLinks);
+console.log(dropdownMenus);
 
 function isMenuOpen() {
   if (!menuOpen) {
@@ -20,4 +23,22 @@ function isMenuOpen() {
   }
 }
 
+function handleDropDown(e) {
+  if (!dropdownOpen) {
+    e.target.children[0].firstElementChild.classList.add('open');
+    e.target.children[1].classList.add('show')
+    dropdownOpen = true;
+    console.log(e);
+  } else {
+    e.target.children[0].firstElementChild.classList.remove('open');
+    e.target.children[1].classList.remove('show')
+    dropdownOpen = false;
+    console.log(e);
+  }
+
+}
+
 hamburger.addEventListener('click', isMenuOpen);
+dropdownLinks.forEach(link => {
+  link.addEventListener('click', handleDropDown);
+})
