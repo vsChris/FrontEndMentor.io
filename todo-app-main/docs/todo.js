f60328cc3b48da1e9f9984342c50203ce3105a28
Chris <chris.legrand10@gmail.com>
2021-10-06 23:08:11 -0400

    edited styles + added js for displaying and handling the todo list

--- todo-app-main/docs/todo.js
+++ todo-app-main/docs/todo.js
@@ -5,10 +5,58 @@ const todoActions = document.querySelectorAll('.action');
 const todoListStatus = document.querySelector('.list-status');
 const todosLeft = document.querySelector('.todo-counter');
 const clearTodo = document.querySelector('.clear-todo-btn');
-console.log(todoList);
-console.log(themeToggle);
-console.log(todoForm);
-console.log(todoActions);
-console.log(todoListStatus);
-console.log(todosLeft);
-console.log(clearTodo);
+const state = [];
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const task = e.srcElement[0].value;
+  const todo = {
+    task,
+    id: Date.now(),
+    complete: false,
+  };
+  e.srcElement[0].value = '';
+  state.push(todo);
+  console.log(`You now have ${state.length} items in sitting in state`);
+  e.target.reset();
+  todoList.dispatchEvent(new CustomEvent('stateUpdated'));
+}
+
+function displayTodos() {
+  const html = state.map(
+    (item) => `<li class="todo draggable" data-id="${item.id}" 
+                data-complete="${item.complete}">
+                <input type="checkbox" class="checkbox" />
+                <p class="todo-text"> ${item.task} </p>
+                <button class="delete-todo-btn"></button>
+                </li>
+             `
+  );
+  todoList.innerHTML = html;
+}
+
+function saveToLocalStorage() {
+  const stateToString = JSON.stringify(state);
+  localStorage.setItem('todos', stateToString);
+}
+
+function restoreFromLocalStorage() {
+  if (localStorage.length) {
+    const restoredItems = JSON.parse(localStorage.getItem('todos'));
+    restoredItems.forEach((item) => state.push(item));
+    displayTodos();
+    updateTodosLeft();
+  }
+}
+
+function updateTodosLeft() {
+  console.log(todosLeft);
+}
+
+restoreFromLocalStorage();
+
+todoForm.addEventListener('submit', handleSubmit);
+todoList.addEventListener('stateUpdated', displayTodos);
+todoList.addEventListener('stateUpdated', saveToLocalStorage);
+todoList.addEventListener('stateUpdate', restoreFromLocalStorage);
+todoList.addEventListener('stateUpdated', updateTodosLeft);
